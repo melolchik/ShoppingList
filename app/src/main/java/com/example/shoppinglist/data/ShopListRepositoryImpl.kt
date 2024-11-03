cc561f0ba656cfb15681336ca3d3fb4c3a894437
Olga Melekhina <o.melehina@smart-fin.ru>
2024-11-03 17:04:28 +0300

    3.5 Data

---
+++ app/src/main/java/com/example/shoppinglist/data/ShopListRepositoryImpl.kt
@@ -0,0 +1,36 @@
+package com.example.shoppinglist.data
+
+import com.example.shoppinglist.domain.ShopItem
+import com.example.shoppinglist.domain.ShopListRepository
+
+class ShopListRepositoryImpl : ShopListRepository {
+
+    private val shopList = mutableListOf<ShopItem>()
+
+    private var autoIncrementalId = 0
+    override fun addShopItem(shopItem: ShopItem) {
+        if(shopItem.id == ShopItem.UNDEFINED_ID) {
+            shopItem.id = autoIncrementalId++
+        }
+        shopList.add(shopItem)
+    }
+
+    override fun deleteShopItem(shopItem: ShopItem) {
+        shopList.remove(shopItem)
+    }
+
+    override fun editShopItem(shopItem: ShopItem) {
+       val oldItem = getShopItem(shopItem.id)
+        shopList.remove(oldItem)
+        addShopItem(shopItem)
+    }
+
+    override fun getShopItem(id: Int): ShopItem {
+        return shopList.find { it.id == id }
+            ?: throw RuntimeException("Element with id $id not found")
+    }
+
+    override fun getShopList(): List<ShopItem> {
+        return shopList.toList()
+    }
+}
